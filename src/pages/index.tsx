3d991d62c8b4d728fccabf006682f4b0f3800d27
Allan Martins <allanmartins@id.uff.br>
2021-02-25 16:13:34 -0300

    dia 03 - challenges component and contexts comm

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,14 @@
+import Head from 'next/head';
+
 import { CompletedChallenges } from "../components/CompletedChallenges";
 import { Countdown } from "../components/Countdown";
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
-
-import Head from 'next/head';
+import { ChallengeBox } from "../components/ChallengeBox";
 
 import styles from "../styles/pages/Home.module.css";
+import React from "react";
+
 
 export default function Home() {
   return (
@@ -22,7 +25,9 @@ export default function Home() {
           <CompletedChallenges />
           <Countdown />
         </div>
-        <div></div>
+        <div>
+          <ChallengeBox/>
+        </div>
       </section>
     </div>
   );

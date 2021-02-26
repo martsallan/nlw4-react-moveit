105f86cdcc87e44aa2826d480c66e1680597d0e5
Allan Martins <allanmartins@id.uff.br>
2021-02-26 18:23:41 -0300

    update time countdown and deploy project

--- src/contexts/CountdownContext.tsx
+++ src/contexts/CountdownContext.tsx
@@ -22,7 +22,7 @@ export const CountdownContext = createContext({} as CountdownContextData);
 export function CountdownProvider({children}: CountdownProviderProps){
   const {startNewChallenge} = useContext(ChallengesContext);
   
-  const [time, setTime] = useState(0.05 * 60);
+  const [time, setTime] = useState(30 * 60);
   const [isActive, setIsActive] = useState(false);
   const [hasFinished, setHasFinished] = useState(false);
 
@@ -37,7 +37,7 @@ export function CountdownProvider({children}: CountdownProviderProps){
     clearTimeout(countdownTimeout);
     setIsActive(false);
     setHasFinished(false);
-    setTime(0.05 * 60);
+    setTime(30 * 60);
   }
 
   useEffect(() => {
